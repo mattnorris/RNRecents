e7de1db7ee32e36bca13acaf4db99ab2a7c73e43
Adam Weeks <adweeks@cisco.com>
2018-05-18 15:26:35 -0400

    feat: display test data

--- App.js
+++ App.js
@@ -3,53 +3,29 @@ import { FlatList, StyleSheet, Text, View } from 'react-native';
 
 import SpacesList from './webex/SpacesList';
 
+import testData from './testData.json';
 export default class App extends React.Component {
-  spacesListSpaces = [
-    {
-      activityText: 'I\'m a direct space!',
-      avatarUrl: "https://1efa7a94ed216783e352-c62266528714497a17239ececf39e9e2.ssl.cf1.rackcdn.com/V1~aebdece11a795253ab6dd74cbcb3a113~WNPdL2bTRpqVQfRKYrJuKA==~110",
-      id: "direct-space-id",
-      lastActivityTime: "Yesterday",
-      name: "Direct Space",
-      type: "direct"
-    },
-    {
-      activityText: 'Hey there!',
-      callStartTime: Date.now(),
-      id: 'jane-doe-space',
-      lastActivityTime: '9:05 PM',
-      latestActivity: {
-        actorName: 'Jane Doe',
-        type: 'post'
-      },
-      isUnread: true,
-      name: 'Webex User',
-      teamColor: 'blue',
-      teamName: 'Best Team'
-    },
-    {
-      activityText: 'Hey there!',
-      callStartTime: Date.now(),
-      id: 'red-team-group-space',
-      lastActivityTime: '9:05 PM',
-      latestActivity: {
-        actorName: 'Jane Doe',
-        type: 'post'
-      },
-      isUnread: true,
-      name: 'Red Team Group Space',
-      teamColor: 'red',
-      teamName: 'Red Team',
-      type: 'group'
-    }
-  ];
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      activeSpace: testData[0].id
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(space) {
+    this.setState({activeSpace: space});
+  }
 
   render() {
     return (
       <View style={styles.container}>
         <SpacesList
-          spaces={this.spacesListSpaces}
+          activeSpaceId={this.state.activeSpace}
+          onClick={this.handleClick}
+          spaces={testData}
         />
       </View>
     );
---
+++ TEST-DATA.md
@@ -0,0 +1,5 @@
+To generate test data, run:
+
+```bash
+npx dummy-json testData.hbs > testData.json
+```
